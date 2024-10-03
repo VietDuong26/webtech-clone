6a391a7f7be1c7b60e9facedfb847812491442a8
Ryiong <viethung1209.work@gmail.com>
2024-10-03 09:48:02 +0700

    Create base cart controller

---
+++ src/com/example/webtech/controller/CartController.java
@@ -0,0 +1,51 @@
+package com.example.webtech.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import com.example.webtech.entity.CartItem;
+import com.example.webtech.service.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class CartController {
+    @Autowired
+    CartService cartService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    ProductService productService;
+    @Autowired
+    SizeService sizeService;
+    @Autowired
+    ColorService colorService;
+
+    @GetMapping("/Cart")
+    String showCart() {
+
+    }
+
+    @RequestMapping("/addToCart")
+    String addToCart() {
+
+    }
+
+    @RequestMapping("/minusItem/{id}")
+    String minusItem() {
+
+    }
+
+    @RequestMapping("/addOneToCart/{id}")
+    String addOneItem() {
+
+    }
+
+    @RequestMapping("/removeAllFromCart/{id}")
+    String removeAll() {
+
+    }
+}
